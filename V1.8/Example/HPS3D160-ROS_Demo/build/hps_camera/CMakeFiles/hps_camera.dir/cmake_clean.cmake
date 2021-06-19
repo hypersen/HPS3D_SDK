4eb9e642d91e3508d0a4a030c3a45393dca59014
kevin <Kevin_wang@hypersen.com>
2021-06-19 11:37:11 +0800

    update Linux **.so

---
+++ V1.8/Example/HPS3D160-ROS_Demo/build/hps_camera/CMakeFiles/hps_camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hps_camera.dir/src/hps_camera.cpp.o"
+  "/home/kevin/eclipse-workspace/HPS3D160-SDK/demo/HPS3D160-ROS_Demo/devel/lib/hps_camera/hps_camera.pdb"
+  "/home/kevin/eclipse-workspace/HPS3D160-SDK/demo/HPS3D160-ROS_Demo/devel/lib/hps_camera/hps_camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hps_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
