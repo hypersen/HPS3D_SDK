10d429e9eba0555d29175af572d266989b866eba
Hypersen <joker_wu@hypersen.com>
2023-02-20 16:03:57 +0800

    Delete English directory
    Delete old demo

--- English/HPS3D_SDK_ROS_Demo/build/hps_camera/CMakeFiles/hps_camera_gennodejs.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hps_camera_gennodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- English/HPS3D_SDK_ROS_Demo/devel/share/common-lisp/ros/hps_camera/msg/_package_measureData.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package hps_camera-msg)
-(cl:export '(POINT_CLOUD_DATA-VAL
-          POINT_CLOUD_DATA
-))
