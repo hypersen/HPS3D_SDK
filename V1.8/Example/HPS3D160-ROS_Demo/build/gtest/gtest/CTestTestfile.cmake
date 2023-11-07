df3533e10fe18880ae1bb2b9df1ac64936354440
Hypersen <joker_wu@hypersen.com>
2023-11-07 14:52:23 +0800

    Delete V1.8/Example/HPS3D160-ROS_Demo directory

--- V1.8/Example/HPS3D160-ROS_Demo/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/kevin/eclipse-workspace/HPS3D160-SDK/demo/HPS3D160-ROS_Demo/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
